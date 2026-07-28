6062f9dccb8254a2e8e86a0729b6ad9a760fcbd9
Chatchawan Koedsawas <121638748+petechatchawan@users.noreply.github.com>
2026-07-28 17:06:43 +0700

    style(water): format Water exports

--- packages/water-core/src/index.ts
+++ packages/water-core/src/index.ts
@@ -2,9 +2,4 @@ export { WaterContractError } from './errors.js';
 export type { WaterError, WaterErrorCode } from './errors.js';
 export { OCEAN_POLICY_V1, WATER_GEOMETRY_EPSILON } from './policy.js';
 export { clipTriangleToSea, wetIntervalForEdge } from './wet-fragment.js';
-export type {
-  TriangleVertex,
-  WetFragment,
-  WetInterval,
-  WetVertex,
-} from './wet-fragment.js';
+export type { TriangleVertex, WetFragment, WetInterval, WetVertex } from './wet-fragment.js';

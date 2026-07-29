6ce7bad920c4eba80ee29993702633eb92828725
Chatchawan Koedsawas <121638748+petechatchawan@users.noreply.github.com>
2026-07-29 22:26:17 +0700

    feat: export road connectivity and mutation APIs

--- packages/road-core/src/index.ts
+++ packages/road-core/src/index.ts
@@ -2,17 +2,36 @@ export {
   BASIC_ROAD_CODE,
   BASIC_ROAD_DEFINITION,
   EMPTY_ROAD_CODE,
+  ROAD_EAST,
+  ROAD_NORTH,
+  ROAD_SOUTH,
+  ROAD_WEST,
+  RoadContractError,
   roadDefinitionForCode,
   roadDefinitionForId,
 } from './contracts.js';
 export type {
+  RoadCellView,
   RoadConnectionMask,
+  RoadContractErrorCode,
   RoadDefinition,
   RoadDefinitionCode,
   RoadDefinitionId,
+  RoadInvalidReason,
+  RoadMutationPlan,
+  RoadMutationReceipt,
   RoadOperation,
+  RoadPlacementEnvironment,
   RoadSnapshot,
+  RoadStrokeInput,
 } from './contracts.js';
+export {
+  occupiedRoadCellViewsInChunk,
+  roadCellPolicyInvalidReason,
+  roadCellViewAt,
+  roadConnectionMaskAt,
+} from './connectivity.js';
+export { commitRoadMutation, planRoadMutation } from './road-mutation.js';
 export {
   createEmptyRoadSnapshot,
   createRoadSnapshot,

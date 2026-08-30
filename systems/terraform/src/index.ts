2efffc5b7a695f15d90b03ba839a5c5c66255c19
Pete Chatchawan <121638748+petechatchawan@users.noreply.github.com>
2026-08-31 01:23:56 +0700

    feat(terraform): expose TF1 brush and strength API

--- systems/terraform/src/index.ts
+++ systems/terraform/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+export {
+  buildBrushFootprint,
+  type TerraformBrushFootprint,
+  type TerraformBrushSize,
+} from "./domain/brush-footprint";
+export { strengthLevels, type TerraformStrength } from "./domain/strength";
